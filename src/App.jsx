688315bc91d87c4cefaf9ade3764486438c90265
Mgabriela-sousa <gabisc1221@gmail.com>
2026-05-28 11:25:31 -0300

    atualizando App com Home

--- src/App.jsx
+++ src/App.jsx
@@ -39,7 +39,7 @@ export default function App() {
   const [isCartOpen, setIsCartOpen] =
     useState(false)
 
-  // ADICIONAR PRODUTO
+
   function addToCart(product) {
     setCartItems([...cartItems, product])
   }
